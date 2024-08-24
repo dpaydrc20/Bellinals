d0a7651ff088c27f08de699ad96c1df2aa2b0a2d
$DPAY_DRC20 <139154450+dpaydrc20@users.noreply.github.com>
2024-08-23 22:07:38 -0700

    Update bellinals.js

--- bellinals.js
+++ bellinals.js
@@ -719,3 +719,6 @@ main().catch(e => {
    let reason = e.response && e.response.data && e.response.data.error && e.response.data.error.message
    console.error(reason ? e.message + ':' + reason : e.message)
 })
+   let reason = e.response && e.response.data && e.response.data.error && e.response.data.error.message
+   console.error(reason ? e.message + ':' + reason : e.message)
+})
